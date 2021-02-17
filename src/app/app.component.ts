a62f3c0ba10e6e72da205610d49a4bbeb0b4f399
David Shore <david@foggymedia.com>
2021-02-17 14:11:47 +0100

    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Test';
+  title = 'Hej';
+  isDisabled = false;
+  num = 0;
+  value = '';
+  name = '';
+
+  add(): void {
+    this.num++;
+  }
+
+  onSave(): void {
+    alert('Saved!');
+  }
+
+
+
+  handleInput(event): void {
+    this.value = event.target.value;
+  }
+
 }
---
+++ src/app/customer.ts
@@ -0,0 +1,10 @@
+// string, number, boolean, null, undefined
+
+export interface Customer {
+  customerNo: number;
+  name: string;
+  address: string;
+  city: string;
+  state: string;
+  country: string;
+}
